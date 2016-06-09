630e282f697c15c2c9c1ecf70a08af4075832b1c
Ben Schwarz <ben@germanforblack.com>
2016-06-09 14:44:51 +1000

    Added everything so far

---
+++ src/assets/README.md
@@ -0,0 +1,13 @@
+# perfmatters
+
+Hey, you downloaded the perfmatters poster! Go you. 🤘
+
+If you're planning on printing this yourself, you’re in luck — It’ll look great as an A4 poster, but will also scale perfectly to fit A3, A2, A1 or even A0. (Set your printer to scale to fit, it’s been designed to fit, no matter how basic your printer might be, it should look great).
+
+## Licence
+
+While this poster is free for you to print at your own discretion, please note that it is licensed under Creative Commons Attribution-NonCommerial 4.0
+
+http://creativecommons.org/licenses/by-nc/4.0/
+
+If you do remix this poster, we’d 100% love to see it — drop an email to hello at calibreapp.com, we’ll send you some swag.
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+'use strict'
+
+let webpack = require('webpack')
+let autoprefixer = require('autoprefixer')
+let HtmlWebpackPlugin = require('html-webpack-plugin')
+let ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  context: __dirname + '/src',
+  entry: {
+    javascript: './index.js'
+  },
+  output: {
+    filename: '[hash].js',
+    path: __dirname + '/public',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false
+      }
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'index.html'
+    }),
+    new ExtractTextPlugin('[hash].css')
+  ],
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }
+    ],
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style', 'css?minimize', 'postcss')
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel-loader'],
+      },
+      {
+        test: /\.svg$/,
+        loaders: [
+          'svg-sprite'
+        ]
+      }
+    ]
+  },
+  postcss: function () {
+    return [autoprefixer]
+  }
+}
